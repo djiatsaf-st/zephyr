ec6991904c4a2d85335790ef3df9d965bcbb7c36
Fabrice DJIATSA <fabrice.djiatsa-ext@st.com>
2026-01-05 15:53:48 +0100

    tests: drivers: disk: disk_access: relax out-of-bounds erase assertion
    The sector erase tests previously asserted for a specific -EINVAL return
    code when attempting to erase past the end of the disk. This is too strict
    for drivers that report a generic I/O error (-EIO) in such situations,
    e.g. the STM32 SDMMC driver, which maps HAL failures to -EIO without
    inspecting the precise root cause.
    
    We can also note that the same logic was used in test_sector_write() and
    test_sector_read().
    
    This change avoids spurious test failures with targets that legitimately
    return non-zero errors other than -EINVAL.
    
    Signed-off-by: Fabrice DJIATSA <fabrice.djiatsa-ext@st.com>

--- tests/drivers/disk/disk_access/src/main.c
+++ tests/drivers/disk/disk_access/src/main.c
@@ -285,14 +285,14 @@ static void test_sector_erase(uint8_t *wbuf, uint8_t *rbuf, uint32_t num_sectors
 
 	/* Try and erase past the last sector */
 	rc = erase_sector_checked(rbuf, disk_sector_count - num_sectors + 1, num_sectors);
-	zassert_equal(rc, -EINVAL,
-		      "Unexpected error code when attempting to erase past end of disk");
+	zassert_not_equal(rc, 0,
+		      "Disk should fail to erase out of sector bounds");
 	rc = erase_sector_checked(rbuf, disk_sector_count + 1, num_sectors);
-	zassert_equal(rc, -EINVAL,
-		      "Unexpected error code when attempting to erase past end of disk");
+	zassert_not_equal(rc, 0,
+		      "Disk should fail to erase out of sector bounds");
 	rc = erase_sector_checked(rbuf, UINT32_MAX, num_sectors);
-	zassert_equal(rc, -EINVAL,
-		      "Unexpected error code when attempting to erase past end of disk");
+	zassert_not_equal(rc, 0,
+		      "Disk should fail to erase out of sector bounds");
 }
 
 /* Test multiple reads in series, and reading from a variety of blocks */
